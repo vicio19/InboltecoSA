b8eebd35ed4a91adf0eee71643d85e97c6686504
Pedro Luis del Castillo <38741752+vicio19@users.noreply.github.com>
2019-04-25 00:24:01 -0400

    Update proses.php

--- modules/notas_de_entrega/proses.php
+++ modules/notas_de_entrega/proses.php
@@ -297,6 +297,68 @@ else {
 			})
 		}
 	</script>
+	<script>
+	function agregar (id)
+		{
+			var precio_venta=document.getElementById('precio_venta_'+id).value;
+			var cantidad=document.getElementById('cantidad_'+id).value;
+			//Inicia validacion
+			if (isNaN(cantidad))
+			{
+			alert('Esto no es un numero');
+			document.getElementById('cantidad_'+id).focus();
+			return false;
+			}
+			if (isNaN(precio_venta))
+			{
+			alert('Esto no es un numero');
+			document.getElementById('precio_venta_'+id).focus();
+			return false;
+			}
+			//Fin validacion
+			
+			$.ajax({
+        type: "POST",
+        url: "./ajax/agregar_cotizador.php",
+        data: "id="+id+"&precio_venta="+precio_venta+"&cantidad="+cantidad,
+		 beforeSend: function(objeto){
+			$("#resultados").html("Mensaje: Cargando...");
+		  },
+        success: function(datos){
+		$("#resultados").html(datos);
+		}
+			});
+		}
+		
+			function eliminar (id)
+		{
+			
+			$.ajax({
+        type: "GET",
+        url: "./ajax/agregar_cotizador.php",
+        data: "id="+id,
+		 beforeSend: function(objeto){
+			$("#resultados").html("Mensaje: Cargando...");
+		  },
+        success: function(datos){
+		$("#resultados").html(datos);
+		}
+			});
+
+		}
+		
+		$("#datos_cotizacion").submit(function(){
+		  var atencion = $("#atencion").val();
+		  var tel1 = $("#tel1").val();
+		  var empresa = $("#empresa").val();
+		  var tel2 = $("#tel2").val();
+		  var email = $("#email").val();
+		  var condiciones = $("#condiciones").val();
+		  var validez = $("#validez").val();
+		  var entrega = $("#entrega").val();
+		 VentanaCentrada('./pdf/documentos/cotizacion_pdf.php?atencion='+atencion+'&tel1='+tel1+'&empresa='+empresa+'&tel2='+tel2+'&email='+email+'&condiciones='+condiciones+'&validez='+validez+'&entrega='+entrega,'Cotizacion','','1024','768','true');
+	 	});
+	</script>
     <div class="row"> 
 	
 
